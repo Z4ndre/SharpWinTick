07d292ebf828cf87ab4159b4ab96a5635ac20906
Z4ndre <148073988+Z4ndre@users.noreply.github.com>
2023-11-26 16:27:11 +0300

    Add files via upload

---
+++ lesson_7/lesson_7/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace lesson_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose ( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose ( );
+            }
+            base.Dispose ( disposing );
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent ( )
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(106, 174);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 69);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(242, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 69);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "00";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(198, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 69);
+            this.label3.TabIndex = 2;
+            this.label3.Text = ":";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightCoral;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(104, 268);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(220, 93);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.ImageLocation = "";
+            this.pictureBox1.Location = new System.Drawing.Point(384, 65);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(528, 436);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.WaitOnLoad = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(969, 544);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ lesson_7/lesson_7/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lesson_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1 ( )
+        {
+            InitializeComponent ( );
+        }
+        int n = 0;
+        int min = 0;
+        int sec = 0;
+        private void button1_Click ( object sender, EventArgs e )
+        {
+            
+            if (button1.Text == "Старт" )
+            {
+                button1.Text = "Стоп";
+                timer1.Start ( );
+            }
+            else
+            {
+                button1.Text = "Старт";
+                timer1.Stop ( );
+            }
+        }
+
+        private void timer1_Tick ( object sender, EventArgs e )
+        {
+            n++;
+            if (n%2 == 0 )
+            {
+                label3.Text = "";
+            }
+            else
+            {
+                label3.Text = ":";
+            }
+            sec++;
+
+            if ( sec < 60 )
+            {
+                if ( sec < 10 )
+                {
+                    label2.Text = "0" + sec.ToString ( );
+                }
+                else
+                {
+                    label2.Text = sec.ToString ( );
+                }
+            }
+            else
+            {
+                label2.Text = "00";
+                sec = 0;
+                min++;
+                if ( min < 10 )
+                {
+                    label1.Text = "0" + min.ToString ( );
+                }
+                else
+                {
+                    label1.Text = min.ToString ( );
+                }
+            }
+            switch ( label1.Text )
+            {
+                case "00":
+                    {
+                        pictureBox1.ImageLocation = @"1.jpg";
+                        break;
+                    }
+                case "08":
+                    {
+                        pictureBox1.ImageLocation = @"2.jpg";
+                        break;
+                    }
+                case "10":
+                    {
+                        pictureBox1.ImageLocation = @"3.jpg";
+                        break;
+                    }
+                case "11":
+                    {
+                        pictureBox1.ImageLocation = @"4.jpg";
+                        break;
+                    }
+                case "16":
+                    {
+                        pictureBox1.ImageLocation = @"5.jpg";
+                        break;
+                    }
+                case "17":
+                    {
+                        pictureBox1.ImageLocation = @"6.jpg";
+                        break;
+                    }
+                case "18":
+                    {
+                        pictureBox1.ImageLocation = @"7.jpg";
+                        break;
+                    }
+                case "23":
+                    {
+                        pictureBox1.ImageLocation = @"1.jpg";
+                        break;
+                    }
+            }
+            if (label1.Text == "23" )
+            {
+                if ( label2.Text == "59" )
+                {
+                    min = 0;
+                    label1.Text = "0" + min.ToString ( );
+                    sec = 0;
+                }
+            }
+        }
+    }
+}
